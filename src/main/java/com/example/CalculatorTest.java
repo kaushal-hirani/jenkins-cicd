16fb6de4729ea9d76f265959db12727fadfecadc
Kaushal Hirani <60251250+kaushal-hirani@users.noreply.github.com>
2025-01-04 11:35:57 +0530

    Updated CalculatorTest.java

--- src/main/java/com/example/CalculatorTest.java
+++ src/main/java/com/example/CalculatorTest.java
@@ -1,7 +1,8 @@
 package com.example;  
 
-import org.junit.Test;  
-import static org.junit.Assert.*;  
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+ 
 
 public class CalculatorTest {  
     private Calculator calculator = new Calculator();  
